42226460b6e84fe816c5e82b6a1d95bacec6c785
feiyuii <34962253+feiyuii@users.noreply.github.com>
2018-01-02 14:25:26 +0800

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,15 @@
+var ap2 = new APlayer({
+    element: document.getElementById('player2'),
+    narrow: true,
+    autoplay: false,
+    showlrc: false,
+    mutex: true,
+    theme: '#e6d0b2',
+    mode: 'circulation',
+    music: {
+        title: 'Preparation',
+        author: 'Hans Zimmer/Richard Harvey',
+        url: 'http://devtest.qiniudn.com/Preparation.mp3',
+        pic: 'http://devtest.qiniudn.com/Preparation.jpg'
+    }
+});
